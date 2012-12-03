bbc68e096d27e46b100464cd8321f8ed268639b7
Kyle Brantley <kyle@averageurl.com>
2012-12-02 19:11:28 -0700

    Added OnkyoRawCLI

---
+++ OnkyoRawCLI/InboundPacketProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace OnkyoRawCLI {
+    class InboundPacketProcessor {
+        private volatile bool _shouldStop = false;
+        private NetworkStream _stream;
+
+        public void SetStream(NetworkStream stream) {
+            if (stream != null)
+                _stream = stream;
+        }
+
+        public void ProcessPackets() {
+            while (!_shouldStop) {
+                InboundPacket packet = new InboundPacket(_stream);
+                ProcessPacket(packet);
+            }
+        }
+
+        public void RequestStop() {
+            _shouldStop = true;
+        }
+
+        private void ProcessPacket(InboundPacket packet) {
+            Console.WriteLine("-->" + packet.Message);
+        }
+    }
+}
---
+++ OnkyoRawCLI/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+/* Name this 'Receiver Whisperer' */
+
+namespace OnkyoRawCLI {
+    class Program {
+        static void Main(string[] args) {
+
+            if (!(args.Length == 1 || args.Length == 2)) {
+                Console.WriteLine("Please provide IP (port optional) of the receiver");
+                Environment.Exit(-1);
+            }
+
+            TcpClient receiverSocket = new TcpClient();
+            int port = 60128;
+            if (args.Length == 2) {
+                port = System.Convert.ToInt32(args[1]);
+            }
+            receiverSocket.Connect(args[0], port);
+            NetworkStream receiverStream = receiverSocket.GetStream();
+
+            InboundPacketProcessor ipp = new InboundPacketProcessor();
+            ipp.SetStream(receiverStream);
+            Thread inboundThread = new Thread(ipp.ProcessPackets);
+            inboundThread.Start();
+
+            bool quitting = false;
+            while (!quitting) {
+                String msg = Console.ReadLine();
+                if (msg == "quit") {
+                    quitting = true;
+                    ipp.RequestStop();
+                    new OutboundPacket(receiverStream, "MVLQSTN");
+                } else {
+                    Console.WriteLine("<--" + msg);
+                    new OutboundPacket(receiverStream, msg);
+                }
+            }
+            inboundThread.Join();
+            receiverSocket.Close();
+        }
+    }
+}
