f73bf4130633f84b11f2f3bc785c5f2d187f6215
Kyle Brantley <kyle@averageurl.com>
2012-12-05 22:30:37 -0700

    Added Onkyo inputs, input event handlers for TestHarness

--- ReceiverWhisperer.Onkyo/OnkyoInputSelector.cs
+++ ReceiverWhisperer.Onkyo/OnkyoInputSelector.cs
@@ -8,16 +8,18 @@ namespace ReceiverWhisperer.Onkyo {
     public class OnkyoInputSelector : InputSelector {
         OnkyoReceiver _receiver;
         private OnkyoInput _currentInput;
+        private List<Input> _validInputs;
         public Input ActiveInput { get { return _currentInput; } set { value.Activate(); } }
 
         public event ReceiverEventHandler OnInputChanged;
 
         public IEnumerable<Input> AvailableInputs {
-            get { throw new NotImplementedException(); }
+            get { if (_validInputs == null) BuildInputs(); return _validInputs; }
         }
 
         public OnkyoInputSelector(OnkyoReceiver receiver) {
             _receiver = receiver;
+            BuildInputs();
         }
 
         public void SwitchInput(OnkyoInput input) {
@@ -33,6 +35,30 @@ namespace ReceiverWhisperer.Onkyo {
                 OnInputChanged = new ReceiverEventHandler(e);
             else
                 OnInputChanged += new ReceiverEventHandler(e);
+        }
+
+        private void BuildInputs() {
+            if (_validInputs == null) {
+                _validInputs = new List<Input>();
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI01, "SLI01"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI02, "SLI02"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI03, "SLI03"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI04, "SLI04"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI05, "SLI05"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI07, "SLI07"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI08, "SLI08"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI10, "SLI10"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI23, "SLI23"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI24, "SLI24"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI25, "SLI25"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI26, "SLI26"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI27, "SLI27"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI28, "SLI28"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI29, "SLI29"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI2A, "SLI2A"));
+                _validInputs.Add(new OnkyoInput(_receiver, this, ReceiverWhisperer.Onkyo.Properties.Resources.SLI2B, "SLI2B"));
+
+            }
         }
     }
 }
--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -7,12 +7,52 @@ using ReceiverWhisperer.Onkyo;
 
 namespace TestHarness {
     class Program {
+
         static void Main(string[] args) {
             Receiver r = new OnkyoReceiver("TX-NR515", "10.254.254.98", 60128);
             r.Connect();
-            Console.WriteLine("Power status: " + r.PowerManagement.IsPowered);
-            r.PowerManagement.IsPowered = true;
+            Console.WriteLine("Connected.");
+            r.VolumeControl.SubscribeVolumeChanges(VolumeChanged);
+            r.VolumeControl.SubscribeMuteChanges(MuteChanged);
+            r.PowerManagement.SubscribeDisplayBrightnessChanges(DisplayBrightnessChanged);
+            r.PowerManagement.SubscribePowerChanges(PowerChanged);
+            r.PowerManagement.SubscribeSleepChanges(SleepChanged);
+            r.Inputs.SubscribeInputChanges(InputChanged);
+            Console.ReadLine();
             r.Disconnect();
+        }
+
+        private static void VolumeChanged(Object sender, ReceiverEventArgs e) {
+            Console.WriteLine("Volume: " + ((VolumeEventArgs)e).Volume);
+        }
+
+        private static void MuteChanged(Object sender, ReceiverEventArgs e) {
+            if (((MuteEventArgs)e).Muted)
+                Console.WriteLine("Muted");
+            else
+                Console.WriteLine("Not Muted");
+        }
+
+        private static void DisplayBrightnessChanged(Object sender, ReceiverEventArgs e) {
+            Console.WriteLine("Display Brightness: " + ((DisplayBrightnessEventArgs)e).Brightness);
+        }
+
+        private static void PowerChanged(Object sender, ReceiverEventArgs e) {
+            if (((PowerEventArgs)e).Powered)
+                Console.WriteLine("Power On");
+            else
+                Console.WriteLine("Power Off");
+        }
+
+        private static void SleepChanged(Object sender, ReceiverEventArgs e) {
+            if (((SleepEventArgs)e).SleepTimer == 0)
+                Console.WriteLine("Sleep off");
+            else
+                Console.WriteLine("Sleep Timer: " + ((SleepEventArgs)e).SleepTimer + " minutes");
+        }
+
+        private static void InputChanged(Object sender, ReceiverEventArgs e) {
+            Console.WriteLine("Active Input: " + ((InputEventArgs)e).Input.Name);
         }
     }
 }
